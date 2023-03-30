03c795a6ff7faa05949073ea09a6ef0429a86724
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-03-30 20:22:35 -0300

    Inclusão da funcionalidade exclusão lógica de médicos e do campo ativo na tabela medico

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -29,6 +29,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
@@ -36,6 +38,7 @@ public class Medico {
         this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
 
     public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
@@ -50,5 +53,9 @@ public class Medico {
         if(dados.endereco() != null){
             this.endereco.atualizaInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
