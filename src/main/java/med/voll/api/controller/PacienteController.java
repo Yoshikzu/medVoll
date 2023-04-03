21c6879dbf02084ccc75500988293a271c0a7530
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-04-03 18:53:08 -0300

    Inclusão dos pacotes infra e domain e reorganização das classese tratamento de erro not found no detalhamento

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
-import med.voll.api.paciente.*;
+import med.voll.api.domain.paciente.*;
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
