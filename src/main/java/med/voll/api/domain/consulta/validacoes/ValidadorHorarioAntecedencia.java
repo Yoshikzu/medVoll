40eba3cb9f9c088aefbe60acc656394746aa0c30
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-04-06 21:29:35 -0300

    Inclusão da Interface ValidadorAgendamentoDeConsultas e aplicação do Principios SOLID

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -2,12 +2,14 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
 
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class ValidadorHorarioAntecedencia {
+@Component
+public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsultas {
 
     public void validar(DadosAgendamentoConsulta dados){
         var dataConsulta = dados.data();
@@ -18,4 +20,5 @@ public class ValidadorHorarioAntecedencia {
             throw new ValidacaoException("Consulta deve ser agendada com antecedencia mínima de 30 minutos.");
         }
     }
+
 }
